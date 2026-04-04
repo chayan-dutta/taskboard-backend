6b131937512e3074ebfc630d4e27af02e076bdfc
Chayan Dutta <chayan.officialmail@gmail.com>
2026-04-04 19:28:09 +0530

    Auth repositories - infrastructure layer

---
+++ TaskBoard/TaskBoard.AuthService/Infrastructure/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using TaskBoard.AuthService.Infrastructure.Entities;
+
+namespace TaskBoard.AuthService.Infrastructure.Repositories;
+
+public interface IUserRepository
+{
+    Task<User?> GetByEmailAsync(string email);
+    Task<User?> GetByIdAsync(Guid id);
+    Task AddAsync(User user);
+    Task SaveChangesAsync();
+}
---
+++ TaskBoard/TaskBoard.AuthService/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskBoard.AuthService.Infrastructure.Database;
+using TaskBoard.AuthService.Infrastructure.Entities;
+
+namespace TaskBoard.AuthService.Infrastructure.Repositories;
+
+public class UserRepository(UserServiceDbContext dbContext) : IUserRepository
+{
+    private readonly UserServiceDbContext _context = dbContext;
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _context.Users
+            .FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task<User?> GetByIdAsync(Guid id)
+    {
+        return await _context.Users
+            .FirstOrDefaultAsync(u => u.Id == id);
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
